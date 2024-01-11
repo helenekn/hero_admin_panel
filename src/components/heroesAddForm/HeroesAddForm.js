c0150be854d8e1f9470ceb1f57e8bea0b6599037
Helen <redkina2508@gmail.com>
2024-01-12 00:07:20 +0200

    fixed comp HeroesAddForm

--- src/components/heroesAddForm/HeroesAddForm.js
+++ src/components/heroesAddForm/HeroesAddForm.js
@@ -12,6 +12,8 @@ import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { v4 as uuidv4 } from "uuid";
 import { heroCreated } from "../heroesList/heroesSlice";
+import { selectAll } from "../heroesFilters/filtersSlice";
+import store from "../../store";
 
 const HeroesAddForm = () => {
   //Состояние для контроля формы
@@ -19,9 +21,8 @@ const HeroesAddForm = () => {
   const [heroDescr, setHeroDescr] = useState("");
   const [heroElement, setHeroElement] = useState("");
 
-  const { filters, filtersLoadingStatus } = useSelector(
-    (state) => state.filters
-  );
+  const { filtersLoadingStatus } = useSelector((state) => state.filters);
+  const filters = selectAll(store.getState());
   const dispatch = useDispatch();
   const { request } = useHttp();
 
